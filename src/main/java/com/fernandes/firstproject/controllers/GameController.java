6aad16e6f8eb529d723857fb58f3d814a05d0509
Lucas Fernandes <luscafernandes027@gmail.com>
2023-07-11 21:32:40 -0300

    Get by ID

--- src/main/java/com/fernandes/firstproject/controllers/GameController.java
+++ src/main/java/com/fernandes/firstproject/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fernandes.firstproject.dto.GameDTO;
 import com.fernandes.firstproject.dto.GameMinDTO;
 import com.fernandes.firstproject.services.GameService;
 
@@ -17,6 +19,14 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+	var result = gameService.findById(id);
+	return result;
+	}
+	
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
